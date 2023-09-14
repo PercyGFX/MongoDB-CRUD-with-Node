a2e4458d815f793aed94d035afab3f677a7e3b8f
PercyGFX <isurangabtk@gmail.com>
2023-09-15 03:07:39 +0530

    mongo connected

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import express from "express";
 const app = express()
 import routes from './routes/routes.js'
+import {connectToDatabase} from './services/db.js'
+import {Db} from 'mongodb'
 
+
+
+
+ 
 // routes
-app.get('/',(req,res)=>{
+app.get('/',async (req,res)=>{
 
-    res.send("helloo")
-
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection('books');
+        const data = await collection.find({}).toArray();
+        res.status(200).json(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    
+   
 })
 
 app.use('/', routes)
