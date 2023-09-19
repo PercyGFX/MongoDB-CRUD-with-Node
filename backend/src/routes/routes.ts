d43db40b746b6376a27bbb94d8351790c79c071f
PercyGFX <isurangabtk@gmail.com>
2023-09-20 01:49:46 +0530

    crud operations done

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -2,10 +2,66 @@ import express from "express";
 const router = express.Router();
 import BookModel from "../models/book.js";
 
+// view books
+
+router.get("/getbooks", (req, res) => {
+  BookModel.find({})
+    .then((book) => {
+      res.json(book);
+    })
+    .catch((err) => console.log(err));
+});
+
+// add new book
 router.post("/addbook", (req, res) => {
   console.log(req.body);
   BookModel.create(req.body)
     .then((book) => res.json(book))
+    .catch((err) => console.log(err));
+});
+
+// get single book
+
+router.get("/getbooks/:id", (req, res) => {
+  const id: string = req.params.id;
+
+  BookModel.findById(id)
+    .then((book) => {
+      res.json(book);
+    })
+    .catch((err) => console.log(err));
+});
+
+//update books
+
+router.put("/updatebook/:id", (req, res) => {
+  const id: string = req.params.id;
+
+  BookModel.findByIdAndUpdate(
+    { _id: id },
+    {
+      bookname: req.body.bookname,
+      author: req.body.author,
+      description: req.body.description,
+      rating: req.body.rating,
+      image: req.body.image,
+    }
+  )
+    .then((book) => {
+      res.json(book);
+    })
+    .catch((err) => console.log(err));
+});
+
+//delete books
+
+router.get("/deletebook/:id", (req, res) => {
+  const id: string = req.params.id;
+
+  BookModel.findByIdAndDelete(id)
+    .then((book) => {
+      res.json(book);
+    })
     .catch((err) => console.log(err));
 });
 
